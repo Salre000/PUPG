7f632dd29618e59fc14c689ef08c7dfd091b6bc9
Salre000 <mcd2376019@stu.o-hara.ac.jp>
2025-04-22 14:03:06 +0900

    オプションクリエイトクラスの定数の場所を変更

---
+++ SabakanField/Assets/Takumi/Const.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Const 
+{
+
+    public static string ClassName = "/Resources/OptionDataClass";
+    public static string FilePass = "/Resources/";
+    public static string CSVName = "OptionDataCSV";
+    public static string ClassExpansion = ".csv";
+
+
+}
