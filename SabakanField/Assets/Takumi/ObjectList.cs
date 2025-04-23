8afc922e1241fe7379328cf1587b44a1c7060036
Salre000 <mcd2376019@stu.o-hara.ac.jp>
2025-04-23 12:49:55 +0900

    恒常武器をオブジェクトに纏めた

--- SabakanField/Assets/Takumi/ObjectList.cs
+++ SabakanField/Assets/Takumi/ObjectList.cs
@@ -5,5 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "MapTile", menuName = "ScriptableObjects/ ObjectList")]
 public class ObjectList : ScriptableObject
 {
+    public List<string>objectName=new List<string>();
     public List<GameObject> objects = new List<GameObject>();
 }
