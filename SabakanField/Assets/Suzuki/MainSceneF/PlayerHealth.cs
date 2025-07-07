292cf4d094f7cfbdab1717293967615654812e43
suzukiyuya <mcd2376083@stu.o-hara.ac.jp>
2025-07-07 14:01:45 +0900

    体力の減少に応じて赤くなるように変更

--- SabakanField/Assets/Suzuki/MainSceneF/PlayerHealth.cs
+++ SabakanField/Assets/Suzuki/MainSceneF/PlayerHealth.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 using TMPro;
 using UnityEngine;
@@ -9,7 +10,12 @@ public class PlayerHealth : MonoBehaviour
     private int _hp = 0;
     [SerializeField]
     private TextMeshProUGUI _text;
-    private StringBuilder stringBuilder=new();
+    private StringBuilder _stringBuilder=new();
+
+    private float _emptySpeed = 1.5f;
+
+    [SerializeField]
+    private Color _emptyColor = Color.red;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +27,7 @@ public class PlayerHealth : MonoBehaviour
     void Update()
     {
         Initialize();
-
+        ColorLerp();
     }
 
     private void Initialize()
@@ -29,8 +35,18 @@ public class PlayerHealth : MonoBehaviour
         // 0==プレイヤーのHP
         _hp = (int)ChracterHPManager.instance.GetHp(0);
         if (_hp < 0) _hp = 0;
-        stringBuilder.Clear();
-        stringBuilder.Append(_hp);
-        _text.text = stringBuilder.ToString();
+        _stringBuilder.Clear();
+        _stringBuilder.Append(_hp);
+        _text.text = _stringBuilder.ToString();
+    }
+
+    // 体力が少なくなるほど体力表示が赤くなる
+    private void ColorLerp()
+    {
+        float current = _hp;
+        float total = ChracterHPManager.instance.GetMaxHp();
+
+        float colorAlpha = (current / total) * _emptySpeed;
+        _text.color = Color.Lerp(_emptyColor, Color.white, colorAlpha);
     }
 }
